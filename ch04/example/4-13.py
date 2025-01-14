bb3c5e9cfae9322d3c71fd9a0cdfd1ade6e44c96
KimMinseong <rjaehno2@naver.com>
2025-01-14 23:11:54 +0900

    Add ch04 examples

---
+++ ch04/example/4-13.py
@@ -0,0 +1,12 @@
+bufets = ('robster', 'sashimi', 'lambs', 'steamed-crab', 'steak')
+
+for bufet in bufets:
+    print(bufet)
+
+# bufets[2] = 'hamburger'
+
+bufets = ('steamed-shellfish', 'sashimi', 'lambs', 'galbi', 'steak')
+
+print("\nModified Menu Items:")
+for bufet in bufets:
+    print(bufet)
