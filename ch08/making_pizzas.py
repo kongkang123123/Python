180df0428888d6febdb87c42b13cbdb3120166a8
KimMinseong <rjaehno2@naver.com>
2025-03-20 00:58:51 +0900

    Add ch08 examples

---
+++ ch08/making_pizzas.py
@@ -0,0 +1,6 @@
+# 모듈: 함수를 따로 저장한 파일
+# import: 불러들이기. 메인 프로그램에서 import해서 모듈을 사용할 수 있다.
+import pizza1
+
+pizza1.make_pizza(16, 'pepperoni')  # `모듈명.함수명` 이런 식으로 사용
+pizza1.make_pizza(12, "mushrooms", "green peppers", 'extra cheese')
